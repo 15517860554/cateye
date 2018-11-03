7c72071b2a340a7b9926423fd56272ee27e12862
sjh <15517860554m1@sina.cn>
2018-11-03 21:24:36 +0800

    b

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,1 +1,21 @@
-console.log(11)
+import React from 'react'
+
+
+
+class Detail extends React.Component{
+	constructor(props){
+		super(props)
+		this.state={
+			
+		}
+	}
+	render(){
+		return(
+			<div>
+				<h1>my detail</h1>
+			</div>
+		)
+	}
+}
+
+export default Detail
